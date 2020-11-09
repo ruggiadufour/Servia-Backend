792e128ceef5a1bdc5c86a6deedc393ef6814d0c
Ruggia <angelruggia@gmail.com>
2020-11-09 16:28:48 -0300

    Prueba subida a servidor 1

--- config/database.js
+++ config/database.js
@@ -1,18 +1,39 @@
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => (
+  {
+    "defaultConnection": "default",
+    "connections": {
+      "default": {
+        "connector": "bookshelf",
+        "settings": {
+          "client": "mysql",
+          "host": "${process.env.DATABASE_HOST}",
+          //"port": "${process.env.DATABASE_PORT}",
+          "database": "${process.env.DATABASE_NAME}",
+          "username": "${process.env.DATABASE_USERNAME}",
+          "password": "${process.env.DATABASE_PASSWORD}",
+          "ssl": { "rejectUnauthorized": false }
+        },
+        "options": {}
+      }
+    }
+  }
+); 
+/*
+{
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3060),
-        database: env('DATABASE_NAME', 'AppServicios_BD'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'root'),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
+        host: env('DATABASE_HOST', 'us-cdbr-east-02.cleardb.com'),
+        //port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'heroku_4d58ad1506b5fd4'),
+        username: env('DATABASE_USERNAME', 'bb855ac93dd138'),
+        password: env('DATABASE_PASSWORD', '8831c6e5'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },//mysql://bb855ac93dd138:8831c6e5@us-cdbr-east-02.cleardb.com/heroku_4d58ad1506b5fd4?reconnect=true
       options: {}
     },
   },
-}); 
+}*/
