85ebd4f9112c9538307586eec319bef9bf04ad4b
Ferran <ferransolischorvat@gmail.com>
2020-11-09 02:09:05 -0300

    Cambios a la BD

---
+++ public/precache-manifest.799d02dca3e7790e9982ce17edf62351.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581b55b4b1580c5498d35de20f687acd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f5e122ad3d72602c49",
+    "url": "/static/css/2.be5d8eb4.chunk.css"
+  },
+  {
+    "revision": "bcca48afc0097e58dc19",
+    "url": "/static/css/main.89838564.chunk.css"
+  },
+  {
+    "revision": "10f5e122ad3d72602c49",
+    "url": "/static/js/2.194594bb.chunk.js"
+  },
+  {
+    "revision": "cee881da2c9c757478cb6c10f8550422",
+    "url": "/static/js/2.194594bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcca48afc0097e58dc19",
+    "url": "/static/js/main.81962db9.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  },
+  {
+    "revision": "f4e4e06bd47045ce741425d0c1997949",
+    "url": "/static/media/Miniatura.f4e4e06b.png"
+  }
+]);
---
+++ public/precache-manifest.d598294172c615a308f81da625684395.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d870057d32c2c305c13ae78e1c1243f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f5e122ad3d72602c49",
+    "url": "/static/css/2.be5d8eb4.chunk.css"
+  },
+  {
+    "revision": "8093f71145dd4efb03b8",
+    "url": "/static/css/main.89838564.chunk.css"
+  },
+  {
+    "revision": "10f5e122ad3d72602c49",
+    "url": "/static/js/2.194594bb.chunk.js"
+  },
+  {
+    "revision": "cee881da2c9c757478cb6c10f8550422",
+    "url": "/static/js/2.194594bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8093f71145dd4efb03b8",
+    "url": "/static/js/main.a0579a2d.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  },
+  {
+    "revision": "f4e4e06bd47045ce741425d0c1997949",
+    "url": "/static/media/Miniatura.f4e4e06b.png"
+  }
+]);
