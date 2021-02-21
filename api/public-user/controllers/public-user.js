fe21dae6aad0b1c2ccd21da8d29cc10efa2f0632
angel ruggia <angelruggia@gmail.com>
2021-02-21 18:51:04 -0300

    Initial changes (models)

---
+++ api/public-user/controllers/public-user.js
@@ -0,0 +1,19 @@
+'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    async filter(ctx) { 
+        let filters = ctx.request.body;
+
+        let query = {}
+        
+        if(filters.category_id)
+        query["categories"] = filters.category_id
+        
+        console.log(query)
+
+        let res = await strapi.query('public-user').find(query);
+        return sanitizeEntity(res, { model: strapi.models['public-user']})
+
+    },
+};
--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'AppServicios'),
+        database: env('DATABASE_NAME', 'servia'),
         username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', '26435'),
+        password: env('DATABASE_PASSWORD', 'root'),
         ssl: env.bool('DATABASE_SSL', true),
       },
       options: {}
